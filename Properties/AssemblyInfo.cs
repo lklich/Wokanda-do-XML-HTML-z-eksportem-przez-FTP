5ad3cc3b04ccd09cdd585c3d1a99ae8b9f280f5c
Leszek Klich <leszek.klich@gmail.com>
2018-12-29 14:50:32 +0100

    Add files via upload

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Ogólne informacje o zestawie są kontrolowane poprzez następujący 
+// zbiór atrybutów. Zmień wartości tych atrybutów by zmodyfikować informacje
+// powiązane z zestawem.
+[assembly: AssemblyTitle("LKWokanda")]
+[assembly: AssemblyDescription("Program eksportu wokand SAWA do XML")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Leszek Klich")]
+[assembly: AssemblyProduct("LKWokanda")]
+[assembly: AssemblyCopyright("Leszek Klich")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Ustawienie wartości ComVisible na false sprawia, że typy w tym zestawie nie będą widoczne 
+// dla składników COM.  Jeśli potrzebny jest dostęp do typu w tym zestawie z 
+// COM, ustaw wartość ComVisible na true, dla danego typu.
+[assembly: ComVisible(false)]
+
+// Następujący GUID jest dla ID typelib jeśli ten projekt jest dostępny dla COM
+[assembly: Guid("d13426d6-c238-413f-b7ef-c19dd249d06e")]
+
+// Informacje o wersji zestawu zawierają następujące cztery wartości:
+//
+//      Wersja główna
+//      Wersja pomocnicza 
+//      Numer kompilacji
+//      Rewizja
+//
+// Można określać wszystkie wartości lub używać domyślnych numerów kompilacji i poprawki 
+// poprzez użycie '*', jak pokazane jest poniżej:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
